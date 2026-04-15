e06408e5c4e42346e1c0444b369839c14ddb8d9a
Laura Camila Rojas Santos <lcrojass@libertadores.edu.co>
2026-04-15 18:16:03 +0000

    Mariposon

--- sketch.js
+++ sketch.js
@@ -1,45 +1,89 @@
 
+let mariposita;
+
+class Mariposita {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+
+  show() {
+    const yMove = sin(frameCount * 0.05) * 15;
+    const scaleFactor = this.size / 100;
+
+    push();
+    translate(this.x, this.y + yMove);
+    scale(scaleFactor);
+    translate(-195, -200);
+
+    fill(123, 63, 0);
+    ellipse(195, 200, 40, 40);
+
+    stroke(255, 255, 255);
+    strokeWeight(19);
+    ellipse(195, 135, 45, 50);
+
+    // 2 petalo
+    fill(135, 206, 235);
+    rotate(4 / 6);
+    ellipse(292, -65, 81, 155);
+    fill(255, 255, 255);
+    push();
+    translate(292, -65);
+    rotate(frameCount * 0.1);
+    ellipse(0, 0, 20, 20);
+    pop();
+
+    // 3 petalo
+    fill(123, 63, 0);
+    rotate(-4 / 6);
+    ellipse(195, 245, 50, 165);
+
+    // 3 petalo
+    fill(135, 206, 235);
+    rotate(-5 / 6);
+    ellipse(-23, 176, 81, 155);
+    fill(255, 255, 255);
+    push();
+    translate(-23, 176);
+    rotate(frameCount * 0.1);
+    ellipse(0, 0, 20, 20);
+    pop();
+
+    // 3 petalo
+    fill(173, 216, 230);
+    rotate(2 / 6);
+    ellipse(111, 346, 75, 120);
+    fill(255, 255, 255);
+    push();
+    translate(111, 346);
+    rotate(frameCount * 0.1);
+    ellipse(0, 0, 20, 20);
+    pop();
+
+    // 2 petalo
+    fill(173, 216, 230);
+    rotate(6 / 6);
+    ellipse(230, 164, 75, 120);
+    fill(255, 255, 255);
+    push();
+    translate(230, 164);
+    rotate(frameCount * 0.1);
+    ellipse(0, 0, 20, 20);
+    pop();
+
+    pop();
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  mariposita = new Mariposita(width / 2, height / 2, 100);
 }
 
 function draw() {
-  background(255,0,220);
-  
-fill(123, 63, 0);
-  
-  ellipse(195, 200, 40, 40);
-  stroke(255, 255, 255);  
-  strokeWeight(19);
- 
-  ellipse(195, 135, 45, 50);
-  
-  
-  //2 petalo
- fill(135, 206, 235);
-  rotate(4/6);
-  ellipse(292,-65, 81, 155);
-  fill(255, 255, 255);
-  ellipse(292, -65, 20, 20);
-  
-  
-  //3 petalo
-  fill(123, 63, 0);
-  rotate(-4/6);
-  ellipse(195,245, 50, 165);
-  
-  //3 petalo
-  fill(135, 206, 235);
-  rotate(-5/6);
-  ellipse(-23,176, 81, 155);
-  
-  //3 petalo
-fill(173, 216, 230);
-  rotate(2/6);
-  ellipse(111,346, 75, 120);
-  
-   //2 petalo
-fill(173, 216, 230);
-  rotate(6/6);
-  ellipse(230,164, 75, 120)
+  background(255, 0, 220);
+  mariposita.show();
 }
+
